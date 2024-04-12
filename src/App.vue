d36c0d23854385ddf90cc8f6bc419722f74e1fdf
Manuel Maisano <manumaisano@gmail.com>
2024-04-12 16:04:27 +0200

    merge

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
- 
+  <HeaderComponent />
+  <MainComponent />
+  <FooterComponent />
 </template>
 
 <script>
+
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
   export default {
-    name: 'App'
+    name: 'App',
+    components:{
+      MainComponent,
+      FooterComponent,
+      HeaderComponent
   }
+}
 </script>
 
 <style lang="scss" scoped>
